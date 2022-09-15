27cb892cb56bb23b5632443afee4179760e9255d
ArtemSpivak <spivak25@yandex.ru>
2022-09-15 21:56:24 +0300

    решены задачи 4-6

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -13,7 +13,7 @@ public class CyclesTheme {
                 oddSum += num;
             }
             num += i;
-        } while(num <= 21);
+        } while (num <= 21);
         System.out.println("В промежутке [-10, 21] сумма четных чисел = " + evenSum +
                 " , а нечетных = " + oddSum);
 
@@ -30,14 +30,14 @@ public class CyclesTheme {
         } else if(num3 > num1 && num3 > num2) {
             max = num3;
         }
-        if(num1 < num2 && num1 < num3) {
+        if (num1 < num2 && num1 < num3) {
             min = num1;
         } else if(num2 < num1 && num2 < num3) {
             min = num2;
         } else if(num3 < num1 && num3 < num2) {
             min = num3;
         }
-        for (i = max; i >= min; i--) {
+        for(i = max; i >= min; i--) {
             System.out.print(i + " ");
         }
 
@@ -51,9 +51,119 @@ public class CyclesTheme {
             reverseNum4 += num4 % 10;
             num4 /= 10;
         }
-        System.out.println("Исходное число в обратном порядке: " + reverseNum4 + ", cумма цифр = " + sum);
+        System.out.println("Исходное число в обратном порядке: " + reverseNum4 + ", cумма цифр = " +
+                sum);
 
         System.out.println("\n4. Вывод чисел на консоль в несколько строк");
+        int num5 = 1;
+        int num6 = 24;
+        int count = 1;
+        for(i = 1; i < num6; i += 2) {
+            if(count > 4) {
+                System.out.printf("%3d%n", i);
+                count = 1;
+            } else {
+                if( i + 2 * (5 - count) > num6) {
+                    System.out.printf("%3d", 0);
+                    i -=2;
+                } else {
+                System.out.printf("%3d", i);
+                }
+                count++;
+            }
+        }
+        System.out.println(" ");
+
+
+        num5 = 1;
+        num6 = 24;
+        count = 1;
+        for(i = 1; i < num6; i += 2) {
+            if(count > 4) {
+                System.out.printf("%3d%n", i);
+                count = 1;
+            } else {
+                System.out.printf("%3d", i);
+                count++;
+                }
+            }
+        if(count <= 6) {
+            for(i = count; i < 6; i++) {
+            System.out.printf("%3d", 0);
+            }
+        }
+        System.out.println(" ");
+
+        System.out.println("\n5. Проверка количества единиц на четность");
+        int num7 = 3141591;
+        int isEven = 0;
+        int evenCount = 0;
+        int oddCount = 0;
+        i = num7;
+        while(i > 0) {
+            isEven = i % 10;
+            if(isEven %2 ==0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+            i /= 10;
+        }
+        System.out.println("Число " + num7 +" содержит " + evenCount +
+                " четное количество единиц, " + oddCount + " нечетное количество единиц");
+
+        System.out.println("\n6. Отображение фигур в консоли");
+        char asterisk = '*';
+        char sharp = '#';
+        char dollar = '$';
+
+        for(i = 0; i < 5; i++) {
+            for(int j = 0; j < 10; j++) {
+                System.out.print("" + asterisk);
+            }
+            System.out.print("\n");
+        }
+
+        i = 0;
+        int j = 0;
+        int num8 = 5;
+        while(i < 5) {
+            while(j < num8){
+                System.out.print("" + sharp);
+                j++;
+            }
+            num8 -= 1;
+            System.out.print("\n");
+            i++;
+            j = 0;
+        }
+
+        i = 0;
+        j = 0;
+        int num9 = 1;
+        do{
+            do{
+                System.out.print(dollar);
+                j++;
+            } while(j < num9);
+            num9++;
+            System.out.print("\n");
+            i++;
+            j = 0;
+        } while (i < 3);
+        i = 0;
+        j = 0;
+        num9 = 2;
+        do{
+            do{
+                System.out.print(dollar);
+                j++;
+            } while(j < num9);
+            System.out.print("\n");
+            num9 -= 1;
+            i++;
+            j = 0;
+        }while(i < 2);
 
     }
 }
