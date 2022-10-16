2e3cb4b34c84ff5fa8923f40793be44dc4b946ae
Artem Spivak <spivak25@yandex.ru>
2022-10-17 00:19:47 +0300

    задание 2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,30 @@
+public class Person() {
+
+    String name = "Инокентий";
+    String sex = "male";
+    int height = 180;
+    double weight 76,5;
+    int age = 56;
+    //идти, сидеть, бежать, говорить, учить Java
+
+     int move() {
+        System.out.println("Сделано много шагов");
+        return 300;
+    }
+
+    void sit() {
+        System.out.println("Кеша сидит");
+    }
+
+    void run() {
+        System.out.println("Режим бега активирован");
+    }
+
+    String speak() {
+        return "Голосовой модуль исправен";
+    }
+}
+
+    void learnJava() {
+        System.out.println("Процесс изучения запущен");
+    }
