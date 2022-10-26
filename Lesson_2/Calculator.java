b7ff577f8c602f91c03f6d187c822ed269ba264c
Artem Spivak <spivak25@yandex.ru>
2022-10-26 17:38:18 +0300

    начато решение задачи с калькулятором

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -3,32 +3,34 @@ public class Calculator {
     private int num1;
     private int num2;
     private char sign = '%';
-    private double result = num1;
 
-    public setNum1(int inputNum){
+    public void setNum1(int inputNum){
         num1 = inputNum;
     }
-    public setNum2(int inputNum){
+    public void setNum2(int inputNum){
         num2 = inputNum;
     }
-    public setSign(char sign) {
+    public void setSign(char sign) {
         this.sign = sign;
     }
 
-    if(sign == '+') {
-        result += num2;
-    } else if(sign == '-') {
-        result -= num2;
-    } else if(sign == '/') {
-        result /= num2;
-    } else if(sign == '*') {
-        result *= num2;
-    } else if(sign == '^') {
-        for(int i = 1; i < num2; i++) {
-            result *= num1;
+    public double calculation(int num1, int num2, char sign){
+        double result = num1;
+        if(sign == '+') {
+            result += num2;
+        } else if(sign == '-') {
+            result -= num2;
+        } else if(sign == '/') {
+            result /= num2;
+        } else if(sign == '*') {
+            result *= num2;
+        } else if(sign == '^') {
+            for(int i = 1; i < num2; i++) {
+                result *= num1;
+            }
+        } else if (sign == '%') {
+            result = (num1 * num2) / 100;
         }
-    } else if (sign == '%') {
-        result = (num1 * num2) / 100;
+        return result;
     }
-    System.out.println(num1 + " " + sign +" " + num2 + " = " + result);
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,11 +1,18 @@
-public class CalculatorTest {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class CalculatorTest {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
         System.out.println("Введите первое число");
-        
+        int num1 = (int) scan.NextInt();
         System.out.println("Введите знак математической операции");
+        char sign = scan.NextLine();
         System.out.println("Введите второе число");
+        int num2 = (int) scan.NextInt();
+
+        System.out.println(num1 + " " + sign +" " + num2 + " = " + calc.calculation(num1, num2, sign));
 
     }
 }
