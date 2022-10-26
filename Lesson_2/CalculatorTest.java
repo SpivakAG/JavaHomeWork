5c67d776caddfe5c316b7c328e8999074898b811
Artem Spivak <spivak25@yandex.ru>
2022-10-26 17:55:25 +0300

    решена проблема со сканером

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -6,11 +6,11 @@ public class CalculatorTest {
         Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
         System.out.println("Введите первое число");
-        int num1 = (int) scan.NextInt();
+        int num1 = (int) scan.nextInt();
         System.out.println("Введите знак математической операции");
-        char sign = scan.NextLine();
+        char sign = scan.next().charAt(0);
         System.out.println("Введите второе число");
-        int num2 = (int) scan.NextInt();
+        int num2 = (int) scan.nextInt();
 
         System.out.println(num1 + " " + sign +" " + num2 + " = " + calc.calculation(num1, num2, sign));
 
