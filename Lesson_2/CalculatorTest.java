8546cc671467ecb424f3739a840e672b285ac821
Artem Spivak <spivak25@yandex.ru>
2022-10-27 11:35:50 +0300

    исправлена задача с калькулятором

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -1,16 +1,35 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
-    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
-        System.out.println("Введите первое число");
-        int num1 = (int) scan.nextInt();
-        System.out.println("Введите знак математической операции");
-        char sign = scan.next().charAt(0);
-        System.out.println("Введите второе число");
-        int num2 = (int) scan.nextInt();
-        System.out.println(num1 + " " + sign +" " + num2 + " = " + calc.calculation(num1, num2, sign));
+        String answer = "";
+        boolean repeat = false;
+        boolean wrongAnswer = false;
+        do {
+            System.out.println("Введите первое число");
+            int num1 = (int) scan.nextInt();
+            System.out.println("Введите знак математической операции");;
+            char sign = scan.next().charAt(0);
+            System.out.println("Введите второе число");
+            int num2 = (int) scan.nextInt();
+            System.out.println(num1 + " " + sign +" " + num2 + " = " + calc.calculation(num1, num2, sign));
+            do {
+                System.out.println("Хотите продолжить вычисления? [yes/no]:");
+                scan.nextLine();
+                answer = scan.nextLine();
+                switch(answer) {
+                    case "yes":
+                        repeat = true;
+                        break;
+                    case "no":
+                        break;
+                    default:
+                        System.out.println("Ответ не распознан, попробуйте еще");
+                        wrongAnswer = true;
+                }
+            } while(wrongAnswer == true);
+        } while(repeat);
     }
 }
