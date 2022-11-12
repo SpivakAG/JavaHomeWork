dfe799563bf34bf8a52cbd752b5bec332e398d7d
Artem Spivak <spivak25@yandex.ru>
2022-11-12 20:53:02 +0300

    исправлен Calculator test

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,26 +4,25 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calc = new Calculator();
-        String answer = "";
+        String answer = "yes";
         do {
-            System.out.println("Введите первое число");
-            int num1 = scan.nextInt();
-            calc.setNum1(num1);
-            System.out.println("Введите знак математической операции");;
-            char sign = scan.next().charAt(0);
-            calc.setSign(sign);
-            System.out.println("Введите второе число");
-            int num2 = scan.nextInt();
-            calc.setNum2(num2);
-            System.out.println(num1 + " " + sign +" " + num2 + " = " + calc.calculation());
-            while(!answer.equals("yes") || !answer.equals("no")) {
-                System.out.println("Хотите продолжить ? [yes/no]:");
+            if(answer.equals("yes")) {
+                System.out.println("Введите первое число");
+                int num1 = scan.nextInt();
+                calc.setNum1(num1);
+                System.out.println("Введите знак математической операции");;
+                char sign = scan.next().charAt(0);
+                calc.setSign(sign);
+                System.out.println("Введите второе число");
+                int num2 = scan.nextInt();
+                calc.setNum2(num2);
                 scan.nextLine();
-                answer = scan.nextLine();
-                if(!answer.equals("yes") || !answer.equals("no")) {
-                    System.out.println("Ответ не распознан, попробуйте еще");
-                }
+                System.out.println(num1 + " " + sign +" " + num2 + " = " + calc.calculation());
+            } else {
+                System.out.println("Ответ не распознан, попробуйте еще");
             }
+            System.out.println("Хотите продолжить? [yes/no]");
+            answer = scan.nextLine();
         } while(!answer.equals("no"));
     }
 }
